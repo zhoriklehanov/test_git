871bba121dcc7e2ed1a35d91d1e24125f8ddff97
lakustovdimka <lakustovdimka@gmail.com>
2025-07-13 23:43:06 +0500

    1234

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     print("Получение погод2ы для Алматы:")
     get_weather(almaty_lat, almaty_lon)
 
-    print("\nВведите свои коорdfdfdasdинаты:")
+    print("\nВведите свои коорdfdasdинаты:")
     try:
         user_lat = float(input("Введите широту (например, 55.75): "))
         user_lon = float(input("Введите долготу (например, 37.61): "))
